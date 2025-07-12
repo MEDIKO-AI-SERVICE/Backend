5a11e2ddd261451f895f87683969b85812048f8d
ishHan <gkswltn3736@gmail.com>
2025-07-13 03:34:19 +0900

    fix: 깃 누락 에러

---
+++ src/main/java/com/mediko/mediko_server/domain/member/domain/infoType/HeightUnit.java
@@ -0,0 +1,7 @@
+package com.mediko.mediko_server.domain.member.domain.infoType;
+
+public enum HeightUnit {
+    M,     // 미터
+    CM,    // 센티미터
+    FT_IN  // 피트-인치
+} 
---
+++ src/main/java/com/mediko/mediko_server/domain/member/domain/infoType/WeightUnit.java
@@ -0,0 +1,7 @@
+package com.mediko.mediko_server.domain.member.domain.infoType;
+
+public enum WeightUnit {
+    KG, // 킬로그램
+    G,  // 그램
+    LB  // 파운드
+} 
